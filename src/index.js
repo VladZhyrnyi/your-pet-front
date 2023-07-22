3288a9b4e7728da61077339b7cda58677a2c9c4a
Oleksii Korolenko <as_vostok@i.ua>
2023-07-22 22:54:48 +0300

    added: theme, ThemeProvider, Buttoms, Modal

--- src/index.js
+++ src/index.js
@@ -4,17 +4,21 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { persistor, store } from 'redux/store';
+import { ThemeProvider } from 'styled-components';
 
 import App from 'components/App';
 import './index.css';
 import './fonts.css';
+import { theme } from 'theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <PersistGate persistor={persistor}>
         <BrowserRouter basename="/your-pet-front">
-          <App />
+          <ThemeProvider theme={theme}>
+            <App />
+          </ThemeProvider>
         </BrowserRouter>
       </PersistGate>
     </Provider>
