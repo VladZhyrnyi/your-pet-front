db0094b1b1f52e7292cc6bf0021fd93520a1568a
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-07-25 18:44:11 +0300

    auth logic

--- src/redux/Auth/authSlice.js
+++ src/redux/Auth/authSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from '@reduxjs/toolkit';
-// import { registerUser, loginUser, logoutUser, refreshUser } from './operations';
+import { registerUser, loginUser, logoutUser, refreshUser } from './operations';
 
 const initialState = {
   user: { name: null, email: null },
@@ -11,7 +11,34 @@ const initialState = {
 export const authSlice = createSlice({
   name: 'auth',
   initialState,
-  extraReducers: {},
+  extraReducers: {
+    [registerUser.fulfilled]: (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.token;
+      state.isLoggedIn = true;
+    },
+    [loginUser.fulfilled]: (state, { payload }) => {
+      state.user = payload.user;
+      state.token = payload.token;
+      state.isLoggedIn = true;
+    },
+    [logoutUser.fulfilled]: state => {
+      state.user = initialState.user;
+      state.token = null;
+      state.isLoggedIn = false;
+    },
+    [refreshUser.pending]: state => {
+      state.isRefreshing = true;
+    },
+    [refreshUser.fulfilled]: (state, { payload }) => {
+      state.user = payload;
+      state.isLoggedIn = true;
+      state.isRefreshing = false;
+    },
+    [refreshUser.rejected]: state => {
+      state.isRefreshing = false;
+    },
+  },
 });
 
 export const authReducer = authSlice.reducer;
--- src/redux/Auth/operations.js
+++ src/redux/Auth/operations.js
@@ -1,10 +1,73 @@
-// import axios from 'axios';
-// import { createAsyncThunk } from '@reduxjs/toolkit';
+import axios from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-// axios.defaults.baseURL = 'https://localhost:3000';
+axios.defaults.baseURL = 'https://localhost:3000';
 
-// const userToken = {
-//   set: token =>
-//     (axios.defaults.headers.common.Authorization = `Bearer ${token}`),
-//   unset: () => (axios.defaults.headers.common.Authorization = ''),
-// };
+const userToken = {
+  set: token =>
+    (axios.defaults.headers.common.Authorization = `Bearer ${token}`),
+  unset: () => (axios.defaults.headers.common.Authorization = ''),
+};
+
+export const registerUser = createAsyncThunk(
+  'auth/register',
+  async (userdata, thunkAPI) => {
+    try {
+      const { data } = await axios.post('api/users/register', userdata);
+
+      userToken.set(data.token);
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const loginUser = createAsyncThunk(
+  'auth/login',
+  async (userdata, thunkAPI) => {
+    try {
+      const { data } = await axios.post('api/users/login', userdata);
+
+      userToken.set(data.token);
+
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const logoutUser = createAsyncThunk(
+  'auth/logout',
+  async (_, thunkAPI) => {
+    try {
+      await axios.post('api/users/logout');
+
+      userToken.unset();
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export const refreshUser = createAsyncThunk(
+  'auth/refresh',
+  async (_, thunkAPI) => {
+    const state = thunkAPI.getState();
+    const persistedToken = state.auth.token;
+
+    if (!persistedToken) {
+      return thunkAPI.rejectWithValue('Unable to fetch user');
+    }
+
+    try {
+      userToken.set(persistedToken);
+      const { data } = await axios.get('api/users/current');
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
