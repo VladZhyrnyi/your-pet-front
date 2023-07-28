f239ce3ff9b606cc5e9db80496a58e65edb1e346
Mozzhakov <imozzhakov@icloud.com>
2023-07-28 19:12:53 +0300

    fixed passwordinput code, added error handler, added logout modal NEED FIXES, added registration modal window

---
+++ src/components/LogoutModal/LogoutModal.jsx
@@ -0,0 +1,32 @@
+import { useDispatch } from 'react-redux';
+import { Modal } from 'components/Modal';
+import { ButtonWhiteBig, ButtonBlueBig } from 'components/Buttons';
+import {
+  LogoutModalContentWrapper,
+  LogoutModalTitle,
+  LogoutModalBtnWrapper,
+} from './LogoutModal.styled';
+import SpriteIcon from 'components/SpriteIcon/SpriteIcon';
+import { logoutUser } from 'redux/Auth/operations';
+
+export const LogoutModal = ({ onClose }) => {
+  const dispatch = useDispatch();
+  const onLogoutBtnClick = () => {
+    console.log('qwe');
+    dispatch(logoutUser);
+    onClose();
+  };
+  return (
+    <Modal closeModal={onClose}>
+      <LogoutModalContentWrapper>
+        <LogoutModalTitle>Already leaving?</LogoutModalTitle>
+        <LogoutModalBtnWrapper>
+          <ButtonWhiteBig prop={onClose}>Cancel</ButtonWhiteBig>
+          <ButtonBlueBig prop={onLogoutBtnClick}>
+            Yes <SpriteIcon icon={'logout'} />
+          </ButtonBlueBig>
+        </LogoutModalBtnWrapper>
+      </LogoutModalContentWrapper>
+    </Modal>
+  );
+};
