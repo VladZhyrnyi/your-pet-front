fd818521d89fa2b1e06952ad28d997a619b348b2
Mis25786 <111245815+Mis25786@users.noreply.github.com>
2023-07-29 20:10:28 +0200

    update render card and open address new page

--- src/components/FriendsList/FriendsList.styled.jsx
+++ src/components/FriendsList/FriendsList.styled.jsx
@@ -4,11 +4,17 @@ export const ListFriends = styled.ul`
   display: flex;
   flex-wrap: wrap;
   gap: 20px;
-  justify-content: center;
+  /* justify-content: space-between; */
   margin-top: 24px;
   margin-bottom: 75px;
 
   @media (min-width: 768px) {
     gap: 32px;
+    margin-top: 40px;
+    margin-bottom: 172px;
+  }
+  @media (min-width: 1280px) {
+    margin-top: 60px;
+    margin-bottom: 69px;
   }
 `;
