0f7e1f62a139b1838c22482e4797924eb7cace51
VladZhyrnyi <vladzhyrnyi@ukr.net>
2023-07-31 22:32:16 +0300

    add modal after verification

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,8 +1,22 @@
 import LoginForm from 'components/LoginForm/LoginForm';
+import Modal from 'components/Modal/Modal';
+
+import { useSearchParams } from 'react-router-dom';
+
 const LoginPage = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const isVerifiedModalshown = Boolean(searchParams.get('verified'));
+
   return (
     <div>
       <LoginForm />
+      {isVerifiedModalshown && (
+        <Modal
+          closeModal={() => setSearchParams(searchParams.delete('verified'))}
+        >
+          Verified
+        </Modal>
+      )}
     </div>
   );
 };
