c81fa42f129c9f78aa7294d56487801d94a3ef56
Vlad Zhyrnyi <50950250+VladZhyrnyi@users.noreply.github.com>
2023-08-02 19:51:57 +0300

    Update operations.js

--- src/redux/Auth/operations.js
+++ src/redux/Auth/operations.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 axios.defaults.baseURL = 'https://your-pet-backend-ni98.onrender.com/api';
-// axios.defaults.baseURL = 'http://localhost:4000/api';
 
 const userToken = {
   set: token =>
