d074d698517d5f47675a1b3134c73b1434568fa7
Hanna Ponomarenko <hannaponomarenko@ukr.net>
2023-08-04 18:31:51 +0300

    clean from comments; location &&  on 102 NoticeCategoryItem.jsx

--- src/redux/User/selectors.js
+++
@@ -1,5 +0,0 @@
-// export const selectMyPets = state => state.user.pets;
-
-// export const selectUserData = state => state.user.user;
-
-// export const selectIsLoadingUser = state => state.user.isLoading;
--- src/redux/store.js
+++ src/redux/store.js
@@ -13,7 +13,6 @@ import storage from 'redux-persist/lib/storage';
 
 import { authReducer } from './Auth/authSlice';
 import { contentReducer } from './Content/contentSlice';
-// import { userReducer } from './User/userSlice';
 
 const authPersistConfig = {
   key: 'auth',
@@ -21,16 +20,10 @@ const authPersistConfig = {
   whitelist: ['token', 'pets'],
 };
 
-// const userPersistConfig = {
-//   key: 'user',
-//   storage,
-// };
-
 export const store = configureStore({
   reducer: {
     auth: persistReducer(authPersistConfig, authReducer),
     content: contentReducer,
-    // user: persistReducer(userPersistConfig, userReducer),
   },
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
